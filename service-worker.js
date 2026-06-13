00c05f964912943e6d1d1a3eb1eb0420b63e3a2d
doctortranslatorai <doctortranslatorai@gmail.com>
2026-06-13 18:02:11 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "aion-presence-v15-notifications";
+const CACHE_NAME = "aion-presence-v16-moderation";
 const FILES_TO_CACHE = ["./","./index.html","./app.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
